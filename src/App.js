b4b4080aab9084bffd5cbb8410db222b43d0acf4
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-03 18:25:05 +0530

    net ninja -4th video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  //
+
+  const title = "welcome to blog created by Abhinaba";
+  const likesCount = 50;
+  // const personDetails = { name: "Abhinaba", age: 30 };
+  const link = "http://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likesCount} times</p>
+        {/* <p>{personDetails}</p> */}
+        <p>{10}</p>
+        <p>{"hello, trekkers"}</p>
+        <p>{[1, 3, 4, 7]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google site link</a>
+      </div>
     </div>
   );
 }
