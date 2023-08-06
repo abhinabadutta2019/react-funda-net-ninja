0e7e723b81f21dcfbb37b418de85ba3a95cca406
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-06 22:24:33 +0530

    7th video ended

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
 const Home = () => {
+  //
+
+  const handleClick = (e) => {
+    console.log("Hello from Abhinaba", e.target);
+  };
+  //
+  //
+  const handleClickClickAgain = (name, e) => {
+    //
+    console.log(`${name}, hello from Abhi..`, e.target);
+  };
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      {/*  */}
+      <button onClick={handleClick}>Click me</button>
+      {/*  */}
+      <button
+        onClick={function (e) {
+          //
+          handleClickClickAgain("Harry👑", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
