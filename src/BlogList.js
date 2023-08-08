f9b8bfdcded85f920ac86440c25697c4a25abbc5
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-09 03:11:51 +0530

    vid -12 for / foreach, in place of filter inside react return

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,36 +1,13 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
-  // props destrucred vs props variable - video 11( end)
-
-  // const blogs = props.blogs;
-
-  // const title = props.title;
-  // const handleDelete = props.handleDelete;
+const BlogList = ({ blogs, title }) => {
   return (
-    //
     <div className="blog-list">
       <h2>{title}</h2>
-      <br />
-      {blogs.map(function (oneBlog) {
-        return (
-          <div className="blog-preview" key={oneBlog.id}>
-            <h4>{oneBlog.title}</h4>
-            <p>written by : {oneBlog.author}</p>
-            {/*  */}
-            <button
-              onClick={
-                //
-                function () {
-                  //
-                  return handleDelete(oneBlog.id);
-                }
-              }
-            >
-              Delete blog
-            </button>
-            <br />
-          </div>
-        );
-      })}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,8 @@
-//
 import { useState } from "react";
 import BlogList from "./BlogList";
-//
 
 const Home = () => {
-  //
   const [blogs, setBlogs] = useState([
-    //
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -16,43 +12,20 @@ const Home = () => {
       id: 3,
     },
   ]);
-  //
-  // for loop
 
-  // const handleDelete = (id) => {
-  //   const newBlogs = [];
-  //   for (let i = 0; i < blogs.length; i++) {
-  //     if (blogs[i].id !== id) {
-  //       newBlogs.push(blogs[i]);
-  //     }
-  //   }
-  //   setBlogs(newBlogs);
-  // };
-  //
-  // foreach
-  //
-  const handleDelete = (id) => {
-    const newBlogs = [];
-    blogs.forEach((blog) => {
-      if (blog.id !== id) {
-        newBlogs.push(blog);
-      }
-    });
-    setBlogs(newBlogs);
-  };
+  const mariosBlogs = [];
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].author === "mario") {
+      mariosBlogs.push(blogs[i]);
+    }
+  }
 
-  //
   return (
     <div className="home">
-      <BlogList
-        blogs={blogs}
-        title="All blogs .."
-        handleDelete={handleDelete}
-      ></BlogList>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={mariosBlogs} title="Mario's Blogs" />
     </div>
   );
 };
-
-//
 
 export default Home;
