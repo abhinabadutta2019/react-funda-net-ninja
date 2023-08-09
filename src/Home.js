0449f98cbaa67efb9dc28fbf1464bbfd68159f8e
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-10 04:55:25 +0530

    video 10 - looping an array, and showing dynamic values of array

--- src/Home.js
+++ src/Home.js
@@ -3,24 +3,32 @@ import { useState } from "react";
 const Home = () => {
   //
 
-  const [name, setName] = useState("Akshay");
-  //
-  const clickEventFunc = function () {
-    //
-    setName("Abir");
-    //
-    console.log(name, "name");
-  };
+  const [myArray, setMyArray] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   //
   return (
     //
     <div className="Home">
-      {/*  */}
-      <p>your name :{name}</p>
-
-      {/*  */}
-      <button onClick={clickEventFunc}>Click here</button>
+      <ul>
+        {myArray.map(function (oneItem) {
+          return (
+            <div key={oneItem.id}>
+              {/*  */}
+              <h4>{oneItem.title}</h4>
+              <p>written by : {oneItem.author}</p>
+            </div>
+          );
+        })}
+      </ul>
     </div>
   );
 };
