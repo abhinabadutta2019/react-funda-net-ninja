e174942aea307f8ce67592f9d12c1d8b938743f3
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-10 00:29:53 +0530

    video 4 tried again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import Navbar from "./Navbar";
-import Home from "./Home";
+// import Navbar from "./Navbar";
+// import Home from "./Home";
 
 function App() {
   //
 
+  const studentName = "akshay";
+  const studentAge = 5;
+  const studentsMarksArr = [4, 5, 7];
+  const link = "https://www.google.com/";
+
+  //
+
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      {/*  */}
-      <div className="content">
-        {/*  */}
-        <Home></Home>
-      </div>
+    <div>
+      <h3>Abhinaba Here</h3>
+      <br />
+      <p>{studentName}</p>
+      <p>{studentAge}</p>
+      <p>[4, 5, 7]</p>
+      <p>{studentsMarksArr}</p>
+      <p>
+        <a href={link}>Click here</a>
+      </p>
     </div>
   );
 }
