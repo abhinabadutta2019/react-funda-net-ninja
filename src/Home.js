223d62178d6a779a6ec38fcd46603ecd5dda0135
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-10 00:10:45 +0530

    made changes saving

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
+//
 import { useState } from "react";
 import BlogList from "./BlogList";
+//
 
 const Home = () => {
+  //
   const [blogs, setBlogs] = useState([
+    //
     { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
     { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
     {
@@ -12,20 +16,43 @@ const Home = () => {
       id: 3,
     },
   ]);
+  //
+  // for loop
 
-  const mariosBlogs = [];
-  for (let i = 0; i < blogs.length; i++) {
-    if (blogs[i].author === "mario") {
-      mariosBlogs.push(blogs[i]);
-    }
-  }
+  // const handleDelete = (id) => {
+  //   const newBlogs = [];
+  //   for (let i = 0; i < blogs.length; i++) {
+  //     if (blogs[i].id !== id) {
+  //       newBlogs.push(blogs[i]);
+  //     }
+  //   }
+  //   setBlogs(newBlogs);
+  // };
+  //
+  //
+  //
+  const handleDelete = (id) => {
+    const newBlogs = [];
+    blogs.forEach((blog) => {
+      if (blog.id !== id) {
+        newBlogs.push(blog);
+      }
+    });
+    setBlogs(newBlogs);
+  };
 
+  //
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-      <BlogList blogs={mariosBlogs} title="Mario's Blogs" />
+      <BlogList
+        blogs={blogs}
+        title="All blogs .."
+        handleDelete={handleDelete}
+      ></BlogList>
     </div>
   );
 };
+
+//
 
 export default Home;
