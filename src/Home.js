bb392fcd27d647eb31a645a1b9193f36fc129149
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-10 03:28:39 +0530

    video 7 here

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,46 @@
 const Home = () => {
   //
 
-  const homeTitle = "Coming from home component";
+  // const homeTitle = "Coming from home component";
+  //
+  const clickEventFunc = function () {
+    //
+    console.log("Hello in console");
+  };
+  //
+  const clicktWithParaFunc = function (name) {
+    //
+    console.log(`Hi ${name}`);
+  };
+  //
+  const clickWithReactEvent = function (e) {
+    //
+    console.log("this is", e.target);
+  };
 
   //
   return (
     //
     <div className="Home">
-      <br />
-      <h3>{homeTitle}</h3>
+      {/*  */}
+      {/* <h3>{homeTitle}</h3> */}
+      <button onClick={clickEventFunc}>click here</button>
+      {/*  */}
+      <button
+        onClick={() => {
+          return clicktWithParaFunc("Rahul bhai");
+        }}
+      >
+        with parameter
+      </button>
+      {/*  */}
+      <button
+        onClick={(e) => {
+          return clickWithReactEvent(e);
+        }}
+      >
+        with react event
+      </button>
     </div>
   );
 };
