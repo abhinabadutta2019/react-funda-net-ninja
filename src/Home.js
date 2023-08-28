ce032d5b7c13647dac27287eb1b517860b09dd75
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-28 22:21:59 +0530

    made some changes as video -7

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,26 @@
 import { useState } from "react";
 
 const Home = () => {
-  const handleClick = () => {
-    console.log("hello abvi");
+  const handleClick = (e) => {
+    console.log("hello abvi", e);
   };
+  //
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
+  //
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain("nain", e);
+        }}
+      >
+        Click Again
+      </button>
     </div>
   );
 };
