faa8f5023bc0c1e17c97b7b4df35fc391aaaac22
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-08-30 21:00:18 +0530

    10th video study

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,30 @@ import { useState } from "react";
 
 const Home = () => {
   //
-  // let name = "mario";
-  const [name, setName] = useState("mario");
-  //
-  //
-  const handleClick = (e) => {
-    setName("luigi");
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   //
 
   //
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
+
+      {blogs.map((blog) => {
+        return (
+          <div key={blog.id}>
+            <h2>{blog.title}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
