eb632f74c0aa9fe746a209efbde3daa03d03fee4
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-02 19:35:32 +0530

    before removing lins- vid 17

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,9 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   //
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   //
-  const [name, setName] = useState("mario");
+
   //
   const deleteButtonFunc = (id) => {
     const newBlogs = blogs.filter((blog) => {
@@ -24,16 +15,14 @@ const Home = () => {
   };
   //
   useEffect(() => {
-    // console.log("use effect ran");
-    // console.log(name);
     fetch("http://localhost:8000/blogs")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         console.log(data);
+        setBlogs(data);
       });
-    //
   }, []);
 
   //
@@ -41,14 +30,13 @@ const Home = () => {
     <div className="home">
       <h2>Homepage</h2>
 
-      <BlogList
-        blogList={blogs}
-        title="All blogs"
-        deleteButtonFunc={deleteButtonFunc}
-      />
-      {/*  */}
-      <button onClick={() => setName("luigi")}>change name</button>
-      <p>{name}</p>
+      {blogs && (
+        <BlogList
+          blogList={blogs}
+          title="All blogs"
+          deleteButtonFunc={deleteButtonFunc}
+        />
+      )}
     </div>
   );
 };
