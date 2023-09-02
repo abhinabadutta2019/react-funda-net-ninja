50ecf3ba4f14a59b47eef56d8950c5980020cf1a
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-02 20:39:05 +0530

    upto video -18

--- src/Home.js
+++ src/Home.js
@@ -5,23 +5,33 @@ const Home = () => {
   //
   const [blogs, setBlogs] = useState(null);
   //
-
+  const [isPending, setIsPending] = useState(true);
   //
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
+    //
+    setTimeout(() => {
+      //
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 1000);
+    //
   }, []);
 
   //
   return (
     <div className="home">
       <h2>Homepage</h2>
+
+      {isPending && <div>Loading ... </div>}
+
+      {/*  */}
 
       {blogs && <BlogList blogList={blogs} title="All blogs" />}
     </div>
