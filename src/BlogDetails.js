085c245c90505d4861bb7cbc9b429641380805f8
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-07 22:34:20 +0530

    vid 26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,14 +1,27 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   //
 
-  const { id } = useParams();
+  const { id } = useParams(); //this is getting the id
+  const { data, error, isPending } = useFetch(
+    `http://localhost:8000/blogs/${id}`
+  );
 
   //
   return (
     <div className="blog-details">
-      <h2>Blog details - {id}</h2>
+      {isPending && <div>Loading...</div>}
+      {/* <h2>Blog details - {id}</h2> */}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2>{data.title}</h2>
+          <p>Written by {data.author}</p>
+          <div>{data.body}</div>
+        </article>
+      )}
     </div>
   );
 };
