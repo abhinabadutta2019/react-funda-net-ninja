6471eca87947a7ee6690734b0c46026f2fdd91e8
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-09-09 02:38:51 +0530

    vid -32

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import BlogDetails from "./BlogDetails";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
+import NotFound from "./NotFound";
 
 function App() {
   //
@@ -23,6 +24,8 @@ function App() {
           {/*  */}
           <Route path="/create" element={<Create />} />
           <Route path="/blogs/:id" element={<BlogDetails />} />
+          {/* 404 page-- it needs to stay in the bottom  */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
         {/* </Switch> */}
       </div>
